75774e0be45ae4e372b029895efe5712628e3b0b
sjmorongell <sjmorongell@gmail.com>
2026-07-10 20:03:55 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './app/App'
+import CityPage from './app/pages/CityPage'
+import { cityData } from './app/data/cityData'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {cityData.map((city) => (
+          <Route
+            key={city.slug}
+            path={`/${city.slug}`}
+            element={<CityPage city={city} />}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
